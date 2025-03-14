c614e628139c5e098a51261a619d54dd1473ca87
lignigno <silentbob@mail.com>
2025-03-14 10:10:42 +0500

    uncomment logout

--- code/templates/code/logout.sh
+++ code/templates/code/logout.sh
@@ -5,5 +5,4 @@
 LIS_SCRIPTS_DIR="/tmp/<user>/code"
 
 $LIS_SCRIPTS_DIR/save.sh
-# osascript -e 'tell application "System Events" to log out'
-echo "just logout"
+osascript -e 'tell application "System Events" to log out'
