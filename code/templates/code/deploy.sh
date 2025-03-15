79aaa2cae2da9ee1c860a6e3bbcfb3121f0aa9c8
lignigno <silentbob@mail.com>
2025-03-15 06:57:05 +0500

    try deploy

--- code/templates/code/deploy.sh
+++ code/templates/code/deploy.sh
@@ -4,7 +4,44 @@ LIS_USER="<user>"
 
 /tmp/$LIS_USER/code/update.sh
 
-rsync -aq --delete /tmp/$LIS_USER/save ~
+LIS_SAVE_DIR=~
+
+LIS_DIRS=($(find /tmp/$LIS_USER/save -mindepth 1 -maxdepth 1 -type d -exec basename {} \;))
+
+# ___________________________________________________________________________SUB FUNCTIONS
+
+save_home_files() {
+	echo $(ls -Ap | grep -v /) $LIS_SAVE_DIR | xargs cp -rf
+}
+
+#                                                                                        |
+# ---------------------------------------------------------------------------------------|
+#                                                                                        |
+
+save_dirs() {
+	for dir in "${LIS_DIRS[@]}"; do
+		if [[ "$dir" == "Library" ]]; then
+			rsync -aq --delete 	--exclude='Trial' \
+								--exclude='Caches' \
+								--exclude='Metadata' \
+								--exclude='Containers' \
+								--exclude='Application Support' \
+								~/$dir $LIS_SAVE_DIR
+			continue
+		elif [[ "$dir" == "Public" || "$dir" == ".Trash" || "$dir" == "Library" ]]; then
+			continue
+		fi
+
+		rsync -aq --delete ~/$dir $LIS_SAVE_DIR
+	done
+}
+
+
+
+cd /tmp/$LIS_USER/save
+
+save_home_files
+save_dirs
 
 echo "alias lis=\"/tmp/$LIS_USER/code/lis.sh\"" >> ~/.zshrc
 
